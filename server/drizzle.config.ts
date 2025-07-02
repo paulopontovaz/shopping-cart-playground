9bf3baaf7d2f1c9f475c018fb65e04aa62791eba
Paulo Vaz <paulopontovaz@gmail.com>
2025-07-02 15:14:55 +0200

    refactor(server): update Drizzle config to try using env variables first

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    dbCredentials: {
-        database: "haiilo_challenge",
-        host: "localhost",
-        password: "postgres",
-        port: 5432,
-        ssl: process.env.DRIZZLE_SSL !== "false",
-        user: "postgres",
-    },
-    dialect: "postgresql",
-    out: "./src/db/migrations",
-    schema: "./src/db/models/index.ts",
+  dbCredentials: {
+    database: process.env.DB_NAME ?? "haiilo_challenge",
+    host: process.env.DB_HOST ?? "localhost",
+    password: process.env.DB_PASS ?? "postgres",
+    port: Number(process.env.DB_PORT) ?? 5432,
+    ssl: process.env.DRIZZLE_SSL !== "false",
+    user: process.env.DB_USER ?? "postgres",
+  },
+  dialect: "postgresql",
+  out: "./src/db/migrations",
+  schema: "./src/db/models/index.ts",
 });
