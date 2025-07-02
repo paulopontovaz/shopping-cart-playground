178266aad5d10cd0a0652d4dc355fe512794d6ff
Paulo Vaz <paulopontovaz@gmail.com>
2025-07-02 19:54:54 +0200

    feat(client): make sure alias works for css files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,21 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    server: {
-        watch: {
-            usePolling: true,
-        },
-        host: true, // needed for the Docker container port mapping to work
-        strictPort: true,
-        port: 3000,
-        allowedHosts: ['client']
+  plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
     },
+    host: true, // needed for the Docker container port mapping to work
+    strictPort: true,
+    port: 3000,
+    allowedHosts: ["client"],
+  },
+  resolve: {
+    alias: { "~": path.resolve(__dirname, "src") },
+  },
 });
