8c9d6ab89babd96729ce907a350b9e712c0defc0
Paulo Vaz <paulopontovaz@gmail.com>
2025-07-02 19:58:54 +0200

    feat(client): implement App component

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,3 +1,22 @@
+import { Outlet } from "react-router";
+
 export const App = () => {
-    return <p>First Implementation</p>;
+  return (
+    <div className="flex flex-col justify-between w-dvw h-dvh">
+      <header className=" p-4 flex justify-between items-center border-b shadow-[0_2px_4px_0_rgb(0,0,0,0.05)] bg-[#b5f2ff]">
+        <img
+          alt="Haiilo Logo"
+          className="max-h-12"
+          src="/assets/images/logo.png"
+        />
+        <nav className="flex justify-between gap-4">
+          <ul>Home</ul>
+          <ul>Back Office</ul>
+        </nav>
+      </header>
+      <section className="p-4 flex-1">
+        <Outlet />
+      </section>
+    </div>
+  );
 };
