66ae01d1958a5da1ec200867816964548b6b6eb2
Paulo Vaz <paulopontovaz@gmail.com>
2025-07-02 19:57:58 +0200

    feat(client): implement GET service for products

---
+++ client/src/api/apiClient.ts
@@ -0,0 +1,8 @@
+import ky from "ky";
+
+const BASE_API_URL =
+  process.env.NODE_ENV === "dev"
+    ? "http://localhost:9000/api"
+    : import.meta.env.VITE_API_BASE_URL;
+
+export const api = ky.extend({ retry: 0, prefixUrl: BASE_API_URL });
---
+++ client/src/api/products/useGetAllProducts.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "~/api/apiClient";
+import type { Product } from "../../../../server/src/db/models/schema";
+
+export type FetchAllProductsResponse = {
+  productList: Product[];
+};
+
+export const fetchAllProductsRequest = async () => {
+  const response = (await api
+    .get("products")
+    .json()) as FetchAllProductsResponse;
+
+  return response.productList ?? [];
+};
+
+export const getAllProductsQueryKey = () => ["products"];
+
+export const useGetAllProducts = () => {
+  const { data } = useQuery({
+    queryKey: getAllProductsQueryKey(),
+    queryFn: fetchAllProductsRequest,
+    retry: 0,
+  });
+
+  return { productList: data ?? [] };
+};
