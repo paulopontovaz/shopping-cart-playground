24ed9c4161875373c389c3f7d77bb959a0e1df07
Paulo Vaz <paulopontovaz@gmail.com>
2025-07-03 16:58:04 +0200

    feat(client): increment useProductForm with requests to the API

--- client/src/components/back-office/product-dialog/product-form/useProductForm.ts
+++ client/src/components/back-office/product-dialog/product-form/useProductForm.ts
@@ -1,43 +1,55 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useCreateProduct } from "~/api/products/useCreateProduct";
+import { useUpdateProduct } from "~/api/products/useUpdateProduct";
+import { getUpdateProperties } from "~/utils/getUpdatedProps";
 import type { Product } from "../../../../../../server/src/db/models/schema";
 
 const productFormSchema = z.object({
   name: z
     .string({ required_error: "Name is required" })
     .min(2, { message: "Name must be at least 2 characters" }),
-  price: z.number({ required_error: "Price is required" }).min(0),
-  offerQuantity: z.number().min(0).int().step(1).optional(),
-  offerPrice: z.number().min(0).optional(),
+  price: z.coerce.number({ required_error: "Price is required" }).min(0),
+  offerQuantity: z.coerce.number().min(0).int().step(1).optional(),
+  offerPrice: z.coerce.number().min(0).optional(),
 });
 
 export type ProductFormSchema = z.infer<typeof productFormSchema>;
 
 type UseProductFormOptions = {
   product: Product | null;
+  onAfterSubmit: () => void;
 };
 
 export const useProductForm = (options: UseProductFormOptions) => {
-  const { product } = options;
+  const { product, onAfterSubmit } = options;
 
+  const { updateProduct } = useUpdateProduct();
+  const { createProduct } = useCreateProduct();
   const productForm = useForm<ProductFormSchema>({
     resolver: zodResolver(productFormSchema),
     defaultValues: {
       name: product?.name ?? "",
-      price: product?.price ?? undefined,
-      offerQuantity: product?.offerQuantity ?? undefined,
-      offerPrice: product?.offerPrice ?? undefined,
+      ...(product?.price && { price: product?.price }),
+      ...(product?.offerQuantity && { offerQuantity: product?.offerQuantity }),
+      ...(product?.offerPrice && { offerPrice: product?.offerPrice }),
     },
   });
 
   const onSubmit = async (values: ProductFormSchema) => {
-    console.log("##### values:", values);
+    if (Object.keys(productForm.formState.errors).length === 0) {
+      if (product) {
+        const updatedProperties = getUpdateProperties(product, values);
+        await updateProduct({
+          id: product.id,
+          ...updatedProperties,
+        });
+      } else {
+        await createProduct(values);
+      }
 
-    if (product) {
-      console.log("##### product:", product);
-    } else {
-      console.log("##### No product");
+      onAfterSubmit();
     }
   };
 
